6921ed1e3d52d4e9cb3afa4589ff3f2b04013857
Christoffer Hennie <christoffer.hennie@outlook.com>
2021-03-19 07:36:40 +0000

    Added new function converting binary to ascii

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Converts ASCII strings to binary (base two)
+import binascii
 
-def convert(string):
+def convert_to_binary(string):
 	string_to_bytearray = bytearray(string, 'utf8')
 	binary_array = []
 	
@@ -10,4 +11,22 @@ def convert(string):
 
 	return binary_array
 
-print_bin = lambda arr: ' '.join([str(bin) for bin in arr])
+def convert_to_string(in_bytes):
+  values = in_bytes.split(' ')
+  string_array = []
+
+  for value in values:
+    intager = int(value, 2)
+    character = chr(intager)
+    string_array.append(character)
+  
+  return string_array
+
+
+print_bin = lambda arr: ' '.join(arr)
+
+a = print_bin(convert_to_binary('Halla'))
+
+print(a)
+print(''.join(convert_to_string(a)))
+
